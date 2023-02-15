797f320764a84cdede835fc1e751ce2c6116bcb8
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-02-15 17:20:53 -0500

    Added shell file to facilate the load test.

---
+++ start_services.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#Jiali on linux
+
+# to run the test with ghz
+ghz --insecure \
+  -n 1000 \
+  -m '{"token":"10"}' \
+  --proto ./greeting.proto \
+  --call greeting.v3.GreetingService/Greeting \
+  0.0.0.0:50051
+
