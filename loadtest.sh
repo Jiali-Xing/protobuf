037791a681452519a68a2fae793e0b92f2299769
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-02-20 18:25:48 -0500

    Added shell file to facilate experiments.

--- loadtest.sh
+++ loadtest.sh
@@ -4,7 +4,7 @@
 # to run the test with ghz
 ghz --insecure \
   -n 1000 \
-  -m '{"token":"10"}' \
+  -m '{"tokens":"10"}' \
   --proto ./greeting.proto \
   --call greeting.v3.GreetingService/Greeting \
   0.0.0.0:50051
