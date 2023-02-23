0db70e6608afd5db7a3131c1a37d4de0c50ab5b8
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-02-22 21:51:46 -0500

    To expriment with the speed of charon, so I added extra tokens and req.

--- loadtest.sh
+++ loadtest.sh
@@ -3,8 +3,8 @@
 
 # to run the test with ghz
 ghz --insecure \
-  -n 1000 \
-  -m '{"tokens":"10"}' \
+  -n 10000 \
+  -m '{"tokens":"100"}' \
   --proto ./greeting.proto \
   --call greeting.v3.GreetingService/Greeting \
   0.0.0.0:50051
