9d5e4db3b2de0ea71871d20a3f812d2ebff4cd01
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-02-24 23:00:39 -0500

    To expriment with ghz, I changed the output format.

--- loadtest.sh
+++ loadtest.sh
@@ -2,10 +2,13 @@
 #Jiali on linux
 
 # to run the test with ghz
-ghz --insecure \
-  -n 10000 \
+
+ghz --insecure -O html -o test.html \
+  -n 50000 \
   -m '{"tokens":"100"}' \
   --proto ./greeting.proto \
+  --load-schedule="step" \
+  --load-start=2000 --load-step=2000 --load-end=6000 --load-step-duration=2s \
   --call greeting.v3.GreetingService/Greeting \
   0.0.0.0:50051
 
