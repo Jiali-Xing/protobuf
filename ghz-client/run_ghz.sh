1934c757dbb31e5424e19d2033696b3116cf1c6e
邢嘉力 Jiali Xing <jiali.xing@protonmail.com>
2023-06-05 19:21:31 -0400

    Run experiment in loops of different n of clients

---
+++ ghz-client/run_ghz.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for x in 1 10 100 1000 5000; do
+    cd /home/ying/Sync/Git/k8s-istio-observe-backend/services/protobuf-grpc
+    bash kill_services.sh
+    bash start_services.sh
+    cd /home/ying/Sync/Git/protobuf/ghz-client
+    go run ./main.go $x
+done
+
